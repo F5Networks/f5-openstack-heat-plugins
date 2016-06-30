9a173348d179ad0c1ec93368427abf4f90225f38
Paul Breaux <p.breaux@f5.com>
2016-06-30 15:36:39 -0600

    Implement save and sync heat plugins
    Issues:
    Fixes #80
    
    Problem:
    Create the two plugins and implement tests.
    
    Analysis:
    Two plugins now exist, with save issuing a save commmand on the device.
    The sync plugin will sync to group and the user must give the device group name
    to sync.
    
    Tests:
    All tests pass

---
+++ test/functional/f5_sys_save/test_sys_save.py
@@ -0,0 +1,50 @@
+# Copyright 2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import os
+import pytest
+from pytest import symbols
+
+from heatclient.exc import HTTPException
+
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def test_create_complete(HeatStack):
+    HeatStack(
+        os.path.join(TEST_DIR, 'success.yaml'),
+        'success_test',
+        parameters={
+            'bigip_ip': symbols.bigip_ip,
+            'bigip_un': symbols.bigip_un,
+            'bigip_pw': symbols.bigip_pw
+        }
+    )
+
+
+def test_create_complete_bad_prop(HeatStack):
+    with pytest.raises(HTTPException) as ex:
+        HeatStack(
+            os.path.join(TEST_DIR, 'bad_property.yaml'),
+            'bad_property',
+            parameters={
+                'bigip_ip': symbols.bigip_ip,
+                'bigip_un': symbols.bigip_un,
+                'bigip_pw': symbols.bigip_pw
+            },
+            expect_fail=True
+        )
+    assert 'Unknown Property bad_property' in ex.value.message

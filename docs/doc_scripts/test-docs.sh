26318ac842fcbb044457334844654270778747a8
Jodie Putrino <j.putrino@f5.com>
2017-07-24 16:15:02 -0600

    Fixes #142

--- docs/doc_scripts/test-docs.sh
+++ docs/doc_scripts/test-docs.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 set -e
-#set -x
+set -x
 
 echo "Installing dependencies"
 pip install --user -r requirements.txt
 
-echo "Building docs and Checking links with Sphinx"
+echo "Building docs with Sphinx"
 make -C docs html
-make -C docs linkcheck
 
 echo "Checking grammar and style"
 write-good `find ./docs -not \( -path ./docs/drafts -prune \) -name '*.rst'` --passive --so --no-illusion --thereIs --cliches
+
+set +e
+echo "Checking links"
+make -C docs linkcheck
